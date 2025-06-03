9bdbe7ef2a6ec651608b3ccef7f6d8313a422602
Jean-Arnaud Ritouret <jean-arnaud.ritouret@cognitedata.com@Jean-Arnauds-MacBook-Pro.local>
2025-06-03 07:51:01 +0200

    update logger script

--- spond_case/logger.py
+++ spond_case/logger.py
@@ -1,6 +1,9 @@
 import logging
 
-def get_logger(name=__name__):
+def get_logger(name: str = __name__) -> logging.Logger:
+    """
+    Get a logger with a level of info including the timestamp, the level of the message and the message itself
+    """
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s"
